f2327fc3b12f947d720dd58b6b062fe768441c48
tommyskott <tommy@borgenfalk.se>
2021-06-24 20:44:21 +0200

    Added footer tpl

---
+++ footer.php
@@ -0,0 +1,9 @@
+      <footer>
+        Footer
+      </footer>
+    </div><!--.wrapper-->
+
+    <?php wp_footer(); ?>
+    <script type="text/javascript" src="<?=get_template_directory_uri();?>/build/app.min.js"></script>
+  </body>
+</html>
