31a4987b7aed0a31a16cb0fee7dfbdbc47a4dd84
tommyskott <tommy@borgenfalk.se>
2022-03-09 00:06:15 +0100

    front-page: render home tpl

--- front-page.php
+++ front-page.php
@@ -1,14 +1,21 @@
 <?php
   get_header();
+  global $mustacheEngine;
+  
 
+  /**
+   * Render main content
+   */
   if( have_posts() ){
     while( have_posts() ){
       the_post();
 
-      the_title();
-      the_content();
+      echo $mustacheEngine->render('home', array(
+        'the_content'         =>  apply_filters( 'the_content', get_the_content() )
+      ));
     }
   }
+
 
   get_footer();
 ?>
