72172812213eaeeac9e0a1bd0e8f814df7baee04
tommyskott <tommy@borgenfalk.se>
2022-03-09 11:01:57 +0100

    functions: clean up, more compact comments

--- functions.php
+++ functions.php
@@ -1,11 +1,17 @@
 <?php
   /**
-   * Composer autoloader
+   * WordPress Theme
    */
+
+  // Define text domain for translations
+  define('TXT_DOMAIN', 'WP_THEME');
+
+
+  // Composer autoloader
   if( file_exists( __DIR__ . '/vendor/autoload.php' ) ){
     require_once __DIR__ . '/vendor/autoload.php';
 
-    // Setup mustache
+    // Setup mustache php template engine
     $mustacheEngine = new Mustache_Engine(array(
       'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/tpls'),
       'entity_flags' => ENT_QUOTES
@@ -13,58 +19,29 @@
   }
 
 
-  /**
-   * Define text domain for translations
-   */
-  define('TXT_DOMAIN', 'WP_THEME');
-
-
-  /**
-   * After Setup Theme
-   */
   add_action( 'after_setup_theme', function(){
-    /**
-     * Load text domain and lang dir
-     */
+    // Load text domain and lang dir
     load_theme_textdomain( TXT_DOMAIN, get_template_directory() . '/languages' );
 
-
-    /**
-     * Load custom editor styles (default: editor-style.css)
-     */
+    // Load custom editor styles (default: editor-style.css)
     add_theme_support('editor-styles');
     add_editor_style();
 
-
-    /**
-     * Add support for post thumbnails
-     */
+    // Add support for post thumbnails
     add_theme_support( 'post-thumbnails' );
 
-
-    /**
-     * Add excerpt support for pages
-     */
+    // Add excerpt support for pages
     add_post_type_support( 'page', 'excerpt' );
 
-
-    /**
-     * Add support for align wide blocks
-     */
+    // Add support for align wide blocks
     add_theme_support( 'align-wide' );
-
-
-    /**
-     * Add custom image sizes
-     */
+    
+    // Add custom image sizes
     add_image_size( 'thumb@2x', 300, 300, true );
     add_image_size( 'medium@2x', 600, 600, false );
     add_image_size( 'large@2x', 2048, 2048, false );
 
-
-    /**
-     * Register nav menus
-     */
+    // Register navs
     register_nav_menus(array(
       'primary'       =>    __( 'Primary nav', TXT_DOMAIN ),
       'secondary'     =>    __( 'Secondary nav', TXT_DOMAIN ),
